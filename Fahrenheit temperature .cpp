2d1943a3ce08dd99ba66d67934ab4fa18178dabd
Dreive <72748624+ghettocrab@users.noreply.github.com>
2024-06-16 22:19:18 +0200

    Add files via upload

---
+++ Fahrenheit temperature .cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float C, F;
+	cout << "Enter the Celsius temperature =";
+	cin >> C;
+	F = (C * 9.0) / 5.0 + 32.0;
+
+	cout << " Fahrenheit temperature : " << F << endl;
+
+	return 0;
+}
