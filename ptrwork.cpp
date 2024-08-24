bd5678069bf39a3fbb8b9f40be48cd91c22e1d77
Dreive <72748624+ghettocrab@users.noreply.github.com>
2024-08-24 20:35:38 +0200

    Add files via upload

---
+++ ptrwork.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int num1 = 5;
+   int num2 = 3;
+   int* num1_ptr = &num1;
+   int* num2_ptr = &num2;
+   int result = num1 * num2;
+   int* num3_ptr = &result;
+
+   cout << num3_ptr << endl;
+   
+   return 0;
+}
