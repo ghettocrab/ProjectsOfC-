5edc3f109f0be30a4797cd62a7808f4b09d4b27b
Dreive <72748624+ghettocrab@users.noreply.github.com>
2024-09-02 10:42:04 +0200

    Add files via upload

---
+++ MirroredArray1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	int size = 3;
+	int* a = new int[size];
+	for (int i = 0; i < size; ++i)
+	{
+		cin >> a[i];
+	}
+	for (int i = 0; i < size / 2; i++)
+	{
+		int n = a[i];
+		a[i] = a[size - 1 - i];
+		a[size - 1 - i] = n;
+	}
+	for (int i = 0; i < size; ++i)
+	{
+		cout << a[i];
+	}
+	return 0;
+}
