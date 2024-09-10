b37deeac018cf5908203f75f2f9b3ae84fa0326a
Dreive <72748624+ghettocrab@users.noreply.github.com>
2024-09-10 06:32:12 +0200

    Update and rename functionwork.sln to functionwork.cpp

---
+++ functionwork.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int Add(int a, int b) {
+	int sum = a + b;
+	return sum;
+}
+int main(int, char* []) {
+	int a;
+	cin >> a;
+	int b;
+	cin >> b;
+	int result = Add(a, b);
+	cout << result << endl;
+	return 0;
+}
