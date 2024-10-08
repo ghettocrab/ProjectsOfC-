1ae0276e87a2597cecfb3825068415ed08e9a4d9
Dreive <72748624+ghettocrab@users.noreply.github.com>
2024-10-08 10:24:48 +0200

    Add files via upload

---
+++ returnofvalues.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int* returnLocalVar(int* pa) {
+	cin >> *pa;
+	return pa;
+}
+int main() {
+	int a = 9;
+	int* ptr_a = returnLocalVar(&a);
+	cout << a << endl;
+	cout << *ptr_a << endl;
+	return 0;
+}
