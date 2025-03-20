2bf44523c1393af8790ab4427f159c810cbfd87b
Dreive <72748624+ghettocrab@users.noreply.github.com>
2025-03-20 21:31:46 +0200

    Add files via upload

---
+++ Largest number.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "Enter two non-negative numbers ";
+	cin >> a >> b;
+	int max = (a > b) ? a : b;
+	cout << "The largest number: " << max;
+	return 0;
+}
